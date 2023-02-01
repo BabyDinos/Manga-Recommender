db5f6442ec2307e36a30cf2ae0e5b1d448c67a30
Marcus Lau <108362808+BabyDinos@users.noreply.github.com>
2023-02-01 14:39:01 -0500

    Update main.py

--- main.py
+++ main.py
@@ -6,11 +6,12 @@ from GetData import Data
 import pyarrow as pa
 import pyarrow.parquet as pq
 
+# GUI | Character synopsis and relation
+
 
 data = Data()
 df = data.getRevisedMangaDataframe('themes')
 theme_matrix = data.getSimilarityMatrix('themes', 32)
-data.saveMatrix(theme_matrix, 'themes', 32)
 df = data.getRevisedMangaDataframe('genres')
 genre_matrix = data.getSimilarityMatrix('genres',32)
 
@@ -27,8 +28,9 @@ def sortMangas(recommendation_titles, sort_by, ascending):
 
     return recommendation['synopsis']
 
-recommendation_titles = getNLargest(theme_matrix, 1000, 'Overlord')
+recommendation_titles = getNLargest(genre_matrix, 1000, 'Black Clover')
 mangas = sortMangas(recommendation_titles, ['score'], [False])
 
 print('|*| '+ mangas.index[0] + ' |*|\n' + mangas[0])
 
+mangas
